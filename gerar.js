5da62f09e4f4889e155afdf13bc455113f08dbc6
thiagopinheeir-tech <thiagopinheeir@gmail.com>
2026-01-13 13:48:31 -0300

    Cleanup: Remove unnecessary files, duplicate pages, old scripts and temporary documentation

--- gerar.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-const jwt = crypto.randomBytes(32).toString('hex');
-const enc = crypto.randomBytes(32).toString('hex');
-console.log('JWT_SECRET=' + jwt);
-console.log('ENCRYPTION_KEY=' + enc);
-const fs = require('fs');
-fs.writeFileSync('jwt-key.txt', jwt);
-fs.writeFileSync('enc-key.txt', enc);
